55dc4afdbdd96ca09d616579f3829766d8087169
SEBASTIEN CHRISTOPHE <sebastienchristophe1@gmail.com>
2022-04-24 14:49:10 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar></NavBar>
+    <b-container>
+      <b-row>
+        <h1>Save the Date</h1>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavBar from "~/components/NavBar";
+export default {
+  name: 'IndexPage'
+}
+</script>
